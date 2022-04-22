623a5e22fce555b61b2185308b26527ba3d45dbf
rifat32 <rifatbilalphilips@gmail.com>
2022-04-22 15:09:18 +0600

    server

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const APPNAME = "Tax";
-export const BACKEND = "http://127.0.0.1:8000";
+export const BACKEND = "https://dotzdemo.xyz/door-laravel";
 // export const BACKEND = "https://dotzdemo.xyz/1startb";
 export const BACKENDAPI = `${BACKEND}/api`;
