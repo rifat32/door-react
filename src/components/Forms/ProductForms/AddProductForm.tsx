4048e871d3fc1eb2bbf92c98d83caeaef159e3ac
rifat32 <rifatbilalphilips@gmail.com>
2022-07-06 23:03:02 +0600

    update

--- src/components/Forms/ProductForms/AddProductForm.tsx
+++ src/components/Forms/ProductForms/AddProductForm.tsx
@@ -631,9 +631,12 @@ const AddProductForm: React.FC<UpdateFormInterface> = (props) => {
 				// resetFunction();
 			})
 			.catch((error) => {
-				console.log(error.response);
-				invalidInputHandler(error.response)
-				ErrorMessage(error.response)
+				console.log(error);
+				if(error.response) {
+					invalidInputHandler(error.response)
+					ErrorMessage(error.response)
+				}
+				
 			});
 	};
 	// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
@@ -739,6 +742,8 @@ const AddProductForm: React.FC<UpdateFormInterface> = (props) => {
 
 			return
 			setFormData(props.value);
+		} else if(props.type == "duplicate") {
+			loadProduct(props.value.id)
 		}
 	}, []);
 	const updateData = () => {
