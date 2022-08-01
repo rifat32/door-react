eb6525258da9678465d8d3d368b22fb1d110ffdb
sami-42 <78957317+sami-42@users.noreply.github.com>
2022-08-01 11:51:20 +0600

    I am commiting after changing the config js

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 export const APPNAME = "Doors";
   // export const BACKEND = "http://127.0.0.1:8000";
-   export const BACKEND = "https://dotzdemo.xyz/door-laravel";
+   //export const BACKEND = "https://dotzdemo.xyz/door-laravel";
+export const BACKEND = "https://67d1-203-89-108-11.ngrok.io/";
 
 export const BACKENDAPI = `${BACKEND}/api`;
